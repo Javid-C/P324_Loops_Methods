7abee3b398be57816a582c842e71a1c5f71af7f3
Javid-C <javidia@code.edu.az>
2022-04-13 19:18:25 +0400

    Initial commit

---
+++ P324_Loops_Methods/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P324_Loops_Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
